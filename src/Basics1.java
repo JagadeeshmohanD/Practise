b2fbe2c665083f20046c5b54acb8a4f33640e4c5
jagadeeshmohand <jagadeeshdm87@gmail.com>
2020-06-11 19:39:04 +0530

    java code

---
+++ src/Basics1.java
@@ -0,0 +1,12 @@
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Welcome to java session");
+
+	}
+
+}
