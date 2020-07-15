f115050fdffd588d10bd31c6eaa9cd951080a591
jagadeeshmohand <jagadeeshdm87@gmail.com>
2020-07-15 16:03:41 +0530

    HashMap java

---
+++ src/coreJavaTraining/HashMapExample.java
@@ -0,0 +1,33 @@
+package coreJavaTraining;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     HashMap<Integer, String> hm = new HashMap<Integer, String>();
+     hm.put(0, "hello");
+     hm.put(1, "Gudbey");
+     hm.put(2, "Morning");
+     hm.put(3, "evening");
+     System.out.println(hm.get(2));
+     hm.remove(2);
+     System.out.println(hm.get(2));
+     Set sn =hm.entrySet();
+     Iterator it=sn.iterator();
+     //hashtable -pass table set collections
+     while(it.hasNext())
+     {
+    	 Map.Entry mp= (Map.Entry)it.next();
+    	 System.out.println(mp.getKey());
+    	 System.out.println(mp.getValue());
+     }
+     
+     
+	}
+
+}
